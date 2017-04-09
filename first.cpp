64bb532424f70a7435a529e305a17a04407f33e9
Hobey <hobeyonecanhobey@gmail.com>
2017-04-09 15:46:19 +0200

    added countdown

--- first.cpp
+++ first.cpp
@@ -5,6 +5,11 @@
 int main (int argc, char** argv) {
     
     printf("Hello, world!\n");
-    
+
+    for (int i = 10; i > 0; i--) {
+        printf("%d ", i);
+    }
+    printf("GO!\n");
+
     return 0;
 }
